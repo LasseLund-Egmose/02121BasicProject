9d40050370fae413b3e632e74be7c8f781b5e0ef
Lasse Lund-Egmose <llund-egmose@hotmail.com>
2020-01-06 10:23:47 +0100

    Added Hello World

---
+++ Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        Group root = new Group();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
