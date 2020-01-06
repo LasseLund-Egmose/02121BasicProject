abed97c131f44bc07fb9434c557c6447dc81a619
Thor Eric Dueholm <s194589@student.dtu.dk>
2020-01-06 13:43:30 +0100

    Add initial ChekcerPiece model

--- CheckerPiece.java
+++ CheckerPiece.java
@@ -1,2 +1,23 @@
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Cylinder;
+
 public class CheckerPiece {
+    protected StackPane pane;
+    protected double size;
+
+    public CheckerPiece(double size){
+        this.size=size;
+        this.pane = new StackPane();
+        setupPiece();
+    }
+
+    protected void setupPiece(){
+        Cylinder cylinder = new Cylinder((this.size*2)/5, 8);
+        this.pane.getChildren().add(cylinder);
+    }
+
+    public Pane getPane(){
+        return this.pane;
+    }
 }
