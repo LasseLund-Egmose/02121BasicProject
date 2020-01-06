6bfc87e95ca32ed4b775177ea6144adea4bbca2e
Thor Eric Dueholm <s194589@student.dtu.dk>
2020-01-06 11:32:28 +0100

    Rotation added to Surfacepane

--- View.java
+++ View.java
@@ -1,3 +1,4 @@
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.PerspectiveCamera;
@@ -7,8 +8,10 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class View extends Application {
 
@@ -23,8 +26,38 @@ public class View extends Application {
 
         StackPane.setAlignment(box, Pos.CENTER);
 
+        //Instantiating RotateTransition class
+        RotateTransition rotate = new RotateTransition();
+
+        //Setting Axis of rotation
+        rotate.setAxis(Rotate.Z_AXIS);
+
+        // setting the angle of rotation
+        rotate.setByAngle(180);
+
+        pane.setOnMouseClicked( e ->{
+            rotate.play();
+        });
+        //setting cycle count of the rotation
+        rotate.setCycleCount(1);
+
+        //Setting duration of the transition
+        rotate.setDuration(Duration.millis(1000));
+
+        //the transition will be auto reversed by setting this to true
+        rotate.setAutoReverse(false);
+
+        //setting Rectangle as the node onto which the
+// transition will be applied
+        rotate.setNode(pane);
+
         pane.getChildren().add(box);
         return pane;
+    }
+
+    protected static void setupRotation(Pane pane) {
+
+
     }
 
     @Override
